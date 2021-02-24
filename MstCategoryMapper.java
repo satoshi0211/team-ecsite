dcde02d0132f875bdb02ce20758e0a271f38ea65
satoshi0211 <f0dcfd1@gmail.com>
2021-02-24 18:24:53 +0900

    チーム開発で作成した商品検索機能

---
+++ MstCategoryMapper.java
@@ -0,0 +1,14 @@
+package jp.co.internous.crocus.model.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import jp.co.internous.crocus.model.domain.MstCategory;
+
+@Mapper
+public interface MstCategoryMapper {
+	@Select("SELECT * FROM mst_category")
+	List<MstCategory> find();
+}
